887eaf57d3c50b1d1fb3b1d319aab5cc2c805f99
Caoqizhi <2391580279@qq.com>
2023-05-06 09:40:26 +0800

    en: init wagmi

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import './index.css';
+import '@/style/index.css';
 import 'antd/dist/reset.css';
 import { BrowserRouter } from 'react-router-dom';
 
